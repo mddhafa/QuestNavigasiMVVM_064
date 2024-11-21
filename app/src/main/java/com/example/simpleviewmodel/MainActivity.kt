ec59a4e115ed7e4b6958e5cd65ae95f7cd16ffb3
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-21 18:59:54 +0700

    menambahkan nim pada class mahasiswa

--- app/src/main/java/com/example/simpleviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/simpleviewmodel/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.simpleviewmodel.ui.theme.SimpleViewModelTheme
+import com.example.simpleviewmodel.ui.viewmodel.Navigasi
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleViewModelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Navigasi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
+++ app/src/main/java/com/example/simpleviewmodel/model/Mahasiswa.kt
@@ -2,6 +2,7 @@ package com.example.simpleviewmodel.model
 
 data class Mahasiswa(
     val nama: String = "",
+    val nim: String = "",
     val gender: String = "",
     val alamat: String = "",
     val noTelpon: String = "",
