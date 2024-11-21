ae3f5fdc99c58a48cd05af20f4fd1f5bba972a32
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-21 18:46:15 +0700

    membuat fungsi navigasi

--- app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
@@ -1,6 +1,21 @@
 package com.example.simpleviewmodel.ui.viewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+) {
+
 }
