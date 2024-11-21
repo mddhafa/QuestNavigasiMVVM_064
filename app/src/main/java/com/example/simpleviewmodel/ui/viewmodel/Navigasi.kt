5314aeb71dd8ae5b0bd20fd75e6110bd002293e3
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-11-21 18:58:19 +0700

    menambahkan composable dan membuat  variabel dan memanggil form detail mahasiswa

--- app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
+++ app/src/main/java/com/example/simpleviewmodel/ui/viewmodel/Navigasi.kt
@@ -8,10 +8,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.simpleviewmodel.model.ListGender
+import com.example.simpleviewmodel.ui.view.DetailMahasiswaView
 import com.example.simpleviewmodel.ui.view.FormMahasiswaView
 
 enum class Halaman{
@@ -41,6 +43,14 @@ fun Navigasi(
                     onSubmitClick = {
                         viewModel.saveDataMhs(it)
                         navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
+            composable(route = Halaman.Data.name) {
+               DetailMahasiswaView (
+                    dataMhs = uiState,
+                    onBackButton = {
+                        navHost.popBackStack()
                     }
                 )
             }
